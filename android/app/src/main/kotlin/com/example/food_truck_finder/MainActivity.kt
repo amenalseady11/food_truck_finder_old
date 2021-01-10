4039a2a71e3879c8a1acd24f106185c9efeb0d73
Ferdinand Hodan <bluesoftware.cz@gmail.com>
2021-01-10 11:50:05 +0100

    ICON and SPLASH

--- android/app/src/main/kotlin/com/example/food_truck_finder/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/food_truck_finder/MainActivity.kt
@@ -2,5 +2,8 @@ package com.example.food_truck_finder
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
